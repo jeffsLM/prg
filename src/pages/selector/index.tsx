7eb6261ee0dc9cd2cdf2b470d6c510ab7e2123de
jeffsLM <jeffes.dev@gmail.com>
2021-10-24 10:51:27 -0300

    remove footer

--- src/pages/selector/index.tsx
+++ src/pages/selector/index.tsx
@@ -1,6 +1,6 @@
 import { Flex } from '@chakra-ui/react'
 import { Header } from '../../components/Header'
-import { Footer } from '../../components/Footer'
+
 import { Carousel } from '../../components/Carousel'
 
 
@@ -9,10 +9,9 @@ export default function Selector() {
     return (
         <Flex direction="column" h="100vh">
             <Header nameModule="Seletor" />
-            <Flex flex="1" direction={["column","row"]} align="center"  overflowX="hidden">
+            <Flex flex="1" direction={["column", "row"]} align="center" overflowX="hidden">
                 <Carousel />
             </Flex>
-            <Footer />
         </Flex>
     );
 }
