4f1e10af89ee9038cb772e6f63b1fe466410455c
jeffsLM <jeffes.dev@gmail.com>
2021-10-23 21:19:18 -0300

    refactoring

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,16 +1,18 @@
-import {Flex} from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
+import { Carousel } from '../../components/Carousel'
+
 
 
 export default function Dashboard() {
     return (
         <Flex direction="column" h="100vh">
             <Header nameModule="Seletor" />
-            <Flex flex="1">
-                teste
-                </Flex>            
-            <Footer/>
+            <Flex flex="1" direction={["column","row"]} align="center"  overflowX="hidden">
+                <Carousel />
+            </Flex>
+            <Footer />
         </Flex>
     );
 }
