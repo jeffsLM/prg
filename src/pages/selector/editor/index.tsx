5a2dd9af248e3d93b5fa001f21f72182a32dcad1
jeffsLM <jeffes.dev@gmail.com>
2021-10-25 20:26:43 -0300

    limit editor screen in 1440px

--- src/pages/selector/editor/index.tsx
+++ src/pages/selector/editor/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Stack, Textarea, VStack } from '@chakra-ui/react'
+import { Flex, Box, Stack, Textarea, VStack, Center } from '@chakra-ui/react'
 import React, { useMemo, useState } from 'react'
 import { BsPlus } from 'react-icons/bs'
 import { FaAngleDoubleRight } from 'react-icons/fa'
@@ -12,61 +12,15 @@ import { RichTextBlock } from '../../../components/RichText'
 
 export default function SagaEditor() {
     return (
-        <Box minH="100vh" >
-            <Header nameModule="Elysium Saga's" />
-            <Flex w="100%" direction="row" mt="10">
-                <Stack
-                    spacing="2"
-                    web
-                    overflow="auto"
-                    maxH={400}
-                    css={{
-                        '&::-webkit-scrollbar': { width: '4px' },
-                        '&::-webkit-scrollbar-track': { width: '6px' },
-                        '&::-webkit-scrollbar-thumb': {
-                            background: `rgba(1,24,40,1)`,
-                            borderRadius: '24px',
-                        },
-                    }}>
-                    <PrgItemMeeting>
-                        Início campanha
-                    </PrgItemMeeting>
-                    <PrgItemMeeting icon={BsPlus}>
-                        Add novo encontro
-                    </PrgItemMeeting>
-                </Stack>
-
-                <VStack flex="1" p="4" justify="center">
-                    <Textarea
-                        w="100%"
-                        maxh="50"
-                        h="20"
-                        variant="filled"
-                        bg="blue.900"
-                        borderColor="blue.900"
-                        resize="none"
-                        colorScheme="blue"
-                        placeholder="Titulo do encontro"
-                        focusBorderColor="blue.500"
-                        fontSize="30"
-                        _hover={{
-                            bgColor: "blue.900",
-                        }}
-                    />
-                    <Textarea
-                        w="100%"
-                        h="300"
-                        variant="filled"
-                        bg="blue.900"
-                        borderColor="blue.900"
-                        resize="none"
-                        colorScheme="blue"
-                        placeholder="Humm algo interresante está saindo...."
-                        focusBorderColor="blue.500"
-
-                        _hover={{
-                            bgColor: "blue.900",
-                        }}
+        <Center>
+            <Box minH="100vh" maxW={1440} w="100%" >
+                <Header nameModule="Elysium Saga's" />
+                <Flex w="100%" direction="row" mt="10">
+                    <Stack
+                        spacing="2"
+                        web
+                        overflow="auto"
+                        maxH={400}
                         css={{
                             '&::-webkit-scrollbar': { width: '4px' },
                             '&::-webkit-scrollbar-track': { width: '6px' },
@@ -74,22 +28,70 @@ export default function SagaEditor() {
                                 background: `rgba(1,24,40,1)`,
                                 borderRadius: '24px',
                             },
-                        }}
-                    />
-                    <Button text="Salvar" w="70%"/>
-                </VStack>
+                        }}>
+                        <PrgItemMeeting>
+                            Início campanha
+                        </PrgItemMeeting>
+                        <PrgItemMeeting icon={BsPlus}>
+                            Add novo encontro
+                        </PrgItemMeeting>
+                    </Stack>
 
-                <Stack spacing="8" web overflow="auto" >
-                    <PrgItemController icon={FaAngleDoubleRight}>
-                        Habilidades e Pericias
-                    </PrgItemController>
-                    <PrgItemController icon={FaAngleDoubleRight}>
-                        Players Config
-                    </PrgItemController>
-                </Stack>
-            </Flex>
+                    <VStack flex="1" p="4" justify="center">
+                        <Textarea
+                            w="100%"
+                            maxh="50"
+                            h="20"
+                            variant="filled"
+                            bg="blue.900"
+                            borderColor="blue.900"
+                            resize="none"
+                            colorScheme="blue"
+                            placeholder="Titulo do encontro"
+                            focusBorderColor="blue.500"
+                            fontSize="30"
+                            _hover={{
+                                bgColor: "blue.900",
+                            }}
+                        />
+                        <Textarea
+                            w="100%"
+                            h="300"
+                            variant="filled"
+                            bg="blue.900"
+                            borderColor="blue.900"
+                            resize="none"
+                            colorScheme="blue"
+                            placeholder="Humm algo interresante está saindo...."
+                            focusBorderColor="blue.500"
+
+                            _hover={{
+                                bgColor: "blue.900",
+                            }}
+                            css={{
+                                '&::-webkit-scrollbar': { width: '4px' },
+                                '&::-webkit-scrollbar-track': { width: '6px' },
+                                '&::-webkit-scrollbar-thumb': {
+                                    background: `rgba(1,24,40,1)`,
+                                    borderRadius: '24px',
+                                },
+                            }}
+                        />
+                        <Button text="Salvar" w="70%" />
+                    </VStack>
+
+                    <Stack spacing="8" web overflow="auto" >
+                        <PrgItemController icon={FaAngleDoubleRight}>
+                            Habilidades e Pericias
+                        </PrgItemController>
+                        <PrgItemController icon={FaAngleDoubleRight}>
+                            Players Config
+                        </PrgItemController>
+                    </Stack>
+                </Flex>
 
 
-        </Box>
+            </Box>
+        </Center>
     )
 }
