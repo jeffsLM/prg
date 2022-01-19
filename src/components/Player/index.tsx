231855e8057bdb41dd62bb6d95f8055b6abaf347
jeffsLM <jeffes.dev@gmail.com>
2022-01-18 21:36:23 -0300

    react: enable points in interface

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -3,7 +3,7 @@ import { Flex, Text, Box, HStack, Grid, FlexProps } from '@chakra-ui/react'
 import { PrgIcon } from '../Design/PrgIcon'
 import { PlayerStatus } from '../PlayerStatus'
 
-interface PlayerStatusProps extends FlexProps {
+interface PlayerProps extends FlexProps {
     hp: number;
     mp: number;
     maxHp: number;
@@ -12,9 +12,10 @@ interface PlayerStatusProps extends FlexProps {
     Class: string;
     statusAtMoment?: string;
     placing: number;
+    points: number;
 }
 
-export function Player({ hp, mp, maxHp, maxMp, PlayerName, placing, Class, onClick }: PlayerStatusProps) {
+export function Player({ points, hp, mp, maxHp, maxMp, PlayerName, placing, Class, onClick }: PlayerProps) {
     return (
         <Flex
             onClick={onClick}
@@ -32,7 +33,7 @@ export function Player({ hp, mp, maxHp, maxMp, PlayerName, placing, Class, onCli
             </Flex>
             <HStack direction="row" spacing="6" w="100%" align="center" >
                 <Text>{placing}</Text>
-                <PrgIcon src="/images/mage@2x.png" boxSize='35px' />
+                <PrgIcon points={points} src="/images/mage@2x.png" boxSize='35px' />
                 <Flex direction="column" >
                     <PlayerStatus value={hp} maxValue={maxHp} colorActivate="green.50" colorDesactive="green.800" />
                     <PlayerStatus value={mp} maxValue={maxMp} colorActivate="blue.300" colorDesactive="blue.800" />
